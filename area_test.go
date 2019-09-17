0fb94065990586bb33ab9a0b2d4897f0f27f3a3f
Stefan Majer <stefan.majer@f-i-ts.de>
2019-09-17 20:53:59 +0200

    gofmt

--- area_test.go
+++ area_test.go
@@ -8,83 +8,83 @@ import (
 func TestArea(t *testing.T) {
 	now := time.Now()
 	tests := []struct {
-		name string
+		name   string
 		start  time.Time
 		end    time.Time
 		points []Point
-		want int64
+		want   int64
 	}{
 		{
-			name: "two points inside timewindow",
+			name:  "two points inside timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now, Value: 100},
-				{Timestamp: now.Add(10*time.Second), Value: 100},
+				{Timestamp: now.Add(10 * time.Second), Value: 100},
 			},
 			want: int64(1000),
 		},
 		{
-			name: "there points inside timewindow",
+			name:  "there points inside timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now, Value: 100},
-				{Timestamp: now.Add(5*time.Second), Value: 200},
-				{Timestamp: now.Add(10*time.Second), Value: 200},
+				{Timestamp: now.Add(5 * time.Second), Value: 200},
+				{Timestamp: now.Add(10 * time.Second), Value: 200},
 			},
 			want: int64(1500),
 		},
 		{
-			name: "four points inside timewindow",
+			name:  "four points inside timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now, Value: 100},
-				{Timestamp: now.Add(2*time.Second), Value: 200}, // 100 * 2sec
-				{Timestamp: now.Add(4*time.Second), Value: 50}, // 200 * 2 sec
-				{Timestamp: now.Add(10*time.Second), Value: 200}, // 50 * 6 sec
+				{Timestamp: now.Add(2 * time.Second), Value: 200},  // 100 * 2sec
+				{Timestamp: now.Add(4 * time.Second), Value: 50},   // 200 * 2 sec
+				{Timestamp: now.Add(10 * time.Second), Value: 200}, // 50 * 6 sec
 			},
 			want: int64(900),
 		},
 		{
-			name: "two points one before one inside timewindow",
+			name:  "two points one before one inside timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now.Add(-2 * time.Second), Value: 100},
-				{Timestamp: now.Add(2*time.Second), Value: 200}, // 100 * 2sec
+				{Timestamp: now.Add(2 * time.Second), Value: 200}, // 100 * 2sec
 			},
 			want: int64(200),
 		},
 		{
-			name: "two points one before one after timewindow",
+			name:  "two points one before one after timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now.Add(-2 * time.Second), Value: 100},
-				{Timestamp: now.Add(12*time.Second), Value: 200}, // 100 * 10sec
+				{Timestamp: now.Add(12 * time.Second), Value: 200}, // 100 * 10sec
 			},
 			want: int64(1000),
 		},
 		{
-			name: "three points one before one inside and one after timewindow",
+			name:  "three points one before one inside and one after timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now.Add(-2 * time.Second), Value: 100},
-				{Timestamp: now.Add(5*time.Second), Value: 200}, // 100 * 5sec
-				{Timestamp: now.Add(12*time.Second), Value: 200}, // 200 * 5sec
+				{Timestamp: now.Add(5 * time.Second), Value: 200},  // 100 * 5sec
+				{Timestamp: now.Add(12 * time.Second), Value: 200}, // 200 * 5sec
 			},
 			want: int64(1500),
 		},
 		{
-			name: "two points outside of timewindow",
+			name:  "two points outside of timewindow",
 			start: now,
-			end: now.Add(10 * time.Second),
+			end:   now.Add(10 * time.Second),
 			points: []Point{
 				{Timestamp: now.Add(-20 * time.Second), Value: 100},
-				{Timestamp: now.Add(-10 *time.Second), Value: 200},
+				{Timestamp: now.Add(-10 * time.Second), Value: 200},
 			},
 			want: int64(0),
 		},
